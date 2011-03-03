0884201f483b59dc3539347ed74de91da59f9ac3
Cody Hill <codyhill@bdogm.com>
2011-03-03 15:52:41 -0600

    Initial commit for demo app using scaffolding. NOTE: this is the first time I've used scaffolding.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ca72277f8509705941b5d81cb7ab6eae15d80b11ba37366fa81f3b8dd3d1ce05d2e7b917a3f74af0944b88d74cd3c498f93b92bd8ba0365e76fa411bd191fb97'
